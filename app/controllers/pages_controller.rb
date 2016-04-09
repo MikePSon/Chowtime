83f9885afa528bc70cbff6c0635f748a0d5eead7
Mike Patterson <mikepatterson@Mikes-MacBook-Pro-2.local>
2016-04-08 22:30:37 -0400

    Main Tiles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,6 +19,56 @@ class PagesController < ApplicationController
     @categories = FoodGroup.all
     @myCart = KitchenItem.where(:user_id => current_user.id).where(:userHas => false)[1..7]
     @myCartLength = KitchenItem.where(:user_id => current_user.id).where(:userHas => false).count
+
+
+
+    @myIngredients = KitchenItem.where(:user_id => current_user.id)
+    @notMyRecipes = Recipe.where.not(:user_id => current_user.id)
+    @youGotThis = []
+    @needToShop = []
+
+    @notMyRecipes.each do |thisNotMine|
+
+      #Get Ingredients, can I make this recipe?
+      recipeIngredients = Ingredient.where(:recipe_id => thisNotMine.id)
+      recipeIngredientsCount = recipeIngredients.count
+
+      #Do I have these Ingredients
+      ingredientsInStock = 0
+      recipeIngredients.each do |thisIngredient|
+        name = thisIngredient.name
+        matches = KitchenItem.where('lower(name) = ?', name.downcase).where(:user_id => current_user.id)
+        if matches.present?
+           ingredientsInStock += 1
+        end
+      end
+
+      #How many Ingredients am I missing?
+      thisRecipeMissingCount = recipeIngredientsCount - ingredientsInStock
+      almostTherePercent = ingredientsInStock.to_f / recipeIngredientsCount.to_f
+
+      #Add to relevant array
+      if thisRecipeMissingCount == 0 #Not missing any recipes
+        @youGotThis.push(thisNotMine)
+      elsif thisRecipeMissingCount > 0 && thisRecipeMissingCount <= 3 #missing less than 3 ingredients
+        @needToShop.push(thisNotMine)
+      end 
+    end #End notMyRecipes.each
+
+
+
+
+
+    @newRecipes = Recipe.where(':created_at > ?', Date.today - 7).count
+    @newUsers = User.where(':created_at > ?', Date.today - 7).count
+    @youCanMake = @youGotThis.count
+    
+
+
+
+
+
+
   end
   
   def styleguide
